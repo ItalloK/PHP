112a163c22e7d1329d0ec513eaa0116911b7208d
ItalloK <itallo016@gmail.com>
2024-09-15 12:34:22 -0300

    att

--- Projetos/Empresa/MDE/3-dbempresa_local_departamento.sql
+++
@@ -1,7 +0,0 @@
--- local departamento
-
-INSERT INTO `local_departamento` VALUES 
-(1,'São Luis'),(2,'Ribamar'),(3,'Bloco A'),(4,'Bloco B'),(5,'Bloco C'),(6,'Bloco D'),(7,'Bloco E'),(8,'Brasília'),(9,'Recife'),
-(10,'Manaus'),(11,'Belém'),(12,'Goiânia'),(13,'Vitória'),(14,'São Luís'),(15,'Maceió'),(16,'Natal'),
-(17,'João Pessoa'),(18,'Campo Grande'),(19,'Teresina'),(20,'Aracaju');
-
--- Projetos/Empresa/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASS', 'root');
-    define('BASE', 'dbempresa');
-
-    $conn = new mysqli(HOST, USER, PASS, BASE);
-?>
